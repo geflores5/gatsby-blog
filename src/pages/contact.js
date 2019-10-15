fff139393fa081348849063b3f3b1f52da00b5ac
Giovanni Flores <geflores@Pip-Boy.local>
2019-10-15 01:09:54 -0700

    add github logo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Head from '../components/head';
+import gitcat from '../images/github.png'
 
 const ContactPage = () => {
     return (
         <Layout>
             <Head title="Contact" />
-            <h1>Connect With Me</h1>
+            <h1>Connect with Me</h1>
             <p>Email: geflores5@gmail.com</p>
             <p>
                 <a href="https://github.com/geflores5" target="_blank" >
-                    github
+                    <img src={gitcat} alt="geflores5"/>
                 </a>
             </p>
         </Layout>
